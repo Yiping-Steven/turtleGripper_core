91069b8aeb7fdaf22258a40c726a6cd0cb4923a0
Yiping (Steven) <yipingz18@jhu.edu>
2021-04-14 22:18:55 -0400

    restructured the code directory and created my actionlib for navi

---
+++ taskMng_module/tgrip_taskMng_srvcli/include/taskMng_srvcli/server.hpp
@@ -0,0 +1,110 @@
+#include <ros/ros.h>
+
+// actions
+#include <tgrip_nav_actions/moveAction.h>
+#include <tgrip_nav_actions/exploreAction.h>
+#include <tgrip_nav_actions/maneuverAction.h>
+#include <tgrip_nav_actions/returnAction.h>
+
+#include <tgrip_man_actions/pickAction.h>
+#include <tgrip_man_actions/placeAction.h>
+
+#include <actionlib/server/simple_action_server.h>
+#include <actionlib/server/simple_action_client.h>
+
+// msgs
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Pose2D.h>
+#include <geometry_msgs/Twist.h>
+
+// utilities
+#include <memory>
+#include <tf/tf.h>
+#include <time.h>
+#include <math.h>
+#include <string>
+#include <vector>
+
+// macros
+#define PI 3.1415926
+
+
+class tgrip_taskMngServer{
+    // task action server
+    typedef actionlib::SimpleActionServer<tgrip_taskMng_actions::meteredPatrolAction> tskMeteredPatrolActionServer;
+    typedef actionlib::SimpleActionServer<tgrip_taskMng_actions::timedPatrolAction> timedPatrolActionServer;
+    // subtask action clients [Nav]
+    typedef actionlib::SimpleActionClient<tgrip_nav_actions::moveAction> navMoveActionClient;
+    typedef actionlib::SimpleActionClient<tgrip_nav_actions::exploreAction> navExploreActionClient;
+    typedef actionlib::SimpleActionClient<tgrip_nav_actions::maneuverAction> navManeuverActionClient;
+    typedef actionlib::SimpleActionClient<tgrip_nav_actions::returnAction> navReturnActionClient;
+    // subtask action clients [Nav]
+    typedef actionlib::SimpleActionClient<tgrip_man_actions::placeAction> manPlaceActionClient;
+    typedef actionlib::SimpleActionClient<tgrip_man_actions::pickAction> manPickActionClient;
+
+private:
+
+    ros::NodeHandle nh;
+    // timer
+    time_t timerMissionStart; // unit in seconds
+    time_t timerPhase1;
+    time_t timerPhase2;
+    time_t timerPhase3;
+    // ......
+
+    // all servers
+    ros::ServiceServer taskMngServer; // serving the USER client
+    std::unique_ptr<tgrip_taskMng_actions::meteredPatrolAction> tskMeteredPatrolASPtr; 
+    std::unique_ptr<tgrip_taskMng_actions::timedPatrolAction>   tsktimedPatrolASPtr; // respond to different user requests. 
+    // all clients
+    std::unique_ptr<navExploreActionClient> navExploreACPtr; // [Nav] doing global exploration
+    std::unique_ptr<navManeuverActionClient> navManeuverACPtr; // [Nav] doing local maneuver
+    std::unique_ptr<navReturnActionClient> navReturnACPtr; // [Nav] returning to bace
+    std::unique_ptr<manPlaceActionClient> manPlaceACPtr; // [Man] doing placing
+    std::unique_ptr<manPickActionClient> manPickACPtr; // [Man] doing picking
+    std::unique_ptr<ros::ServiceClient > cvIdClientPtr; //[CV] requesting detection resulty
+
+    // [Nav]
+    // actions
+    tgrip_nav_actions::navExploreGoal navExploreGoal; // explore for a distance, time, location or a coverage (advanced) 
+    tgrip_nav_actions::navExploreFeedback navExploreFeedback;
+    tgrip_nav_actions::navExploreResult navExploreResult;
+    tgrip_nav_actions::navManeuverGoal navManeuverGoal; // maneuver for an angle range
+    tgrip_nav_actions::navManeuverFeedback navManeuverFeedback;
+    tgrip_nav_actions::navManeuverResult navManeuverResult;
+    tgrip_nav_actions::navReturnGoal navReturnGoal; // return to a base location
+    tgrip_nav_actions::navReturnFeedback navReturnFeedback;
+    tgrip_nav_actions::navReturnResult navReturnResult;
+    // all series
+    ros::Publisher pubCmd; // for publishing movement command
+    ros::Subscriber subOdom; // for updating the real time pose.
+    geometry_msgs::Pose2D poseCurrent; // store the real time pose.
+
+    // [Man]
+    // actions
+    tgrip_nav_actions::manPickGoal manPickGoal; // pick for a 3D pose
+    tgrip_nav_actions::manPickFeedback manPickFeedback;
+    tgrip_nav_actions::manPickResult manPickResult;
+    tgrip_nav_actions::manPlaceGoal manPlaceGoal; // place for a 3D pose
+    tgrip_nav_actions::manPlaceFeedback manPlaceFeedback;
+    tgrip_nav_actions::manPlaceResult manPlaceResult;
+    // all series (Jian, add whatever you want here.)
+
+    // [CV]
+    std::vector<cv_msgs::target>  targetList; // create a target message type that stores the pose and certainty 
+    // all series (Irina, add whatever you want here)
+
+
+
+public:
+
+    tgrip_taskMngServer(ros::NodeHandle& nh); 
+
+    void init();
+
+    void run(tgrip_taskMng_srvcli::serveUser::Request &req, 
+                tgrip_taskMng_srvcli::serveUser::Response &res);
+
+    void odomCallback(const tgrip_nav_msgs::Odometry::ConstPtr& odomMsgPtr);
+    
+};
