ee63de0a35a82586fce3641b96f22c0154b41038
yh0298 <wldks456@gmail.com>
2021-05-11 21:03:00 -0400

    open manipulator description update

--- man_module/tgrip_manip_srvcli/src/server.cpp
+++ man_module/tgrip_manip_srvcli/src/server.cpp
@@ -27,39 +27,31 @@ void server::goalCB(){
    
     const robot_state::JointModelGroup *joint_model_group = group.getCurrentState()->getJointModelGroup("arm");
 
-    // group.setPlannerId("RRTConnect");
-
-    // std::cout<<po.pose.pose<<std::endl;
-    // geometry_msgs::Twist pub_topic;
-
-    // //std::cout<<po.pose.pose.position.x<<std::endl;
     tgrip_manip_actions::pick_placeGoal goal = *(as->acceptNewGoal());
-    // std::cout<<"get client "<<"  "<<goal.goal<<std::endl;
-    
-    // float tol_p = goal.goal.position_error;
-    // float tol_a = goal.goal.angular_error;
+    tgrip_manip_actions::pick_placeFeedback feedback;
+    tgrip_manip_actions::pick_placeResult result;
 
-    // float new_x = goal.goal.position.data[0];
-    // float new_y = goal.goal.position.data[1];
-    // float new_z = goal.goal.position.data[2];
 ///////////////////////////////////////////////////////////////////////////////////move to z offset     
-    group.setEndEffectorLink("link5");
+    group.setEndEffectorLink("end_effector_link");
     group.getCurrentState();
     group.setStartStateToCurrentState();
 
     geometry_msgs::Pose target_pose1;
+   
 
     target_pose1.position.x = goal.goal.pose.position.x;//0.124;
     target_pose1.position.y = goal.goal.pose.position.y;//0;
-    target_pose1.position.z = goal.goal.pose.position.z+0.1;//0.3;
+    target_pose1.position.z = goal.goal.pose.position.z+0.2;//0.3;
     target_pose1.orientation.x = goal.goal.pose.orientation.x;
     target_pose1.orientation.y = goal.goal.pose.orientation.y;
     target_pose1.orientation.z = goal.goal.pose.orientation.z;
     target_pose1.orientation.w = goal.goal.pose.orientation.w;
 
+    std::cout<<goal.goal.pose.position<<std::endl;
+    std::cout<<target_pose1.position<<std::endl;
 
-    group.setPositionTarget(target_pose1.position.x,  target_pose1.position.y, target_pose1.position.z, "link5");
-    // group.setPoseTarget(target_pose1, "link5");
+    //group.setPositionTarget(target_pose1.position.x,  target_pose1.position.y, target_pose1.position.z, "link5");
+    group.setPoseTarget(target_pose1, "end_effector_link");
     group.setApproximateJointValueTarget(target_pose1,"end_effector_link");// only works with this function
     group.setGoalTolerance(0.05);
 
@@ -69,15 +61,25 @@ void server::goalCB(){
     moveit::planning_interface::MoveItErrorCode success = group.plan(my_plan);
     // ROS_INFO("Visualizing plan 1 (pose goal) %s", success.val ? "" : "FAILED");
           //If cannot find plan
-    if (!success)
+    if (!success){
       ROS_INFO("fail to move above");
-          //If find plan      
-    group.move(); //blocking
-    sleep(5.0);
+      feedback.feedback_code.feedback_code = "fail to move above";
+      result.result.result_code = "plan fail";
+      as->publishFeedback(feedback);
+      as->setAborted(result);
+      
+    }
+    else{//If find plan
+      feedback.feedback_code.feedback_code = "move to z_offset";
+      as->publishFeedback(feedback);  
+      group.move(); //blocking
+      sleep(5.0);
+    }
 
+  
 ///////////////////////////////////////////////////////////////////////////////////move to real position        
-    group.getCurrentState();
-    group.setStartStateToCurrentState();
+    // group.getCurrentState();
+    // group.setStartStateToCurrentState();
 
     geometry_msgs::Pose target_pose;
 
@@ -89,21 +91,33 @@ void server::goalCB(){
     target_pose.orientation.z = goal.goal.pose.orientation.z;
     target_pose.orientation.w = goal.goal.pose.orientation.w;
 
-    // group.setPoseTarget(target_pose, "end_effector_link");
-    group.setPositionTarget(target_pose.position.x,  target_pose.position.y, target_pose.position.z, "link5");
-    group.setApproximateJointValueTarget(target_pose,"link5");// only works with this function
+    std::cout<<goal.goal.pose.position<<std::endl;
+    std::cout<<target_pose.position<<std::endl;
+
+    group.setPoseTarget(target_pose, "end_effector_link");
+    // group.setPositionTarget(target_pose.position.x,  target_pose.position.y, target_pose.position.z, "link5");
+    group.setApproximateJointValueTarget(target_pose,"end_effector_link");// only works with this function
     // group.setGoalTolerance(0.05);
 
 
     success = group.plan(my_plan);
     // ROS_INFO("Visualizing plan 2 (pose goal) %s", success.val ? "" : "FAILED");
           //If cannot find plan
-    if (!success)
-      ROS_INFO("fail to move point");
-        // throw std::runtime_error("No plan found");
-          //If find plan      
-    group.move(); //blocking
-    sleep(5.0);
+
+    if (!success){
+      feedback.feedback_code.feedback_code = "fail to the cube position";
+      result.result.result_code = "plan fail";
+      as->publishFeedback(feedback);
+      as->setAborted(result);
+ 
+    }
+    else{//If find plan
+      feedback.feedback_code.feedback_code = "move to the cube position";
+      as->publishFeedback(feedback);  
+      group.move(); //blocking
+      sleep(5.0);
+    }
+    
 // ///////////////////////////////////////////////////////////////////////////////////pick      
 
   std::vector<double> gripper_value;
@@ -113,28 +127,34 @@ void server::goalCB(){
 
   success = (group_g.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
   // ROS_INFO_NAMED("tutorial", "gripper", success ? "" : "FAILED");
-
-  if (!success)
-      ROS_INFO("fail to pick");
-  else
-    group_g.move(); //blocking
-    sleep(5.0);
-    ROS_INFO("pick");
+    if (!success){
+      feedback.feedback_code.feedback_code = "fail to pick";
+      result.result.result_code = "plan fail";
+      as->publishFeedback(feedback);
+      as->setAborted(result);
+      
+    }
+    else{//If find plan
+      feedback.feedback_code.feedback_code = "pick";
+      as->publishFeedback(feedback);  
+      group_g.move(); //blocking
+      sleep(5.0);
+    }
 
 // ///////////////////////////////////////////////////////////////////////////////////
-    group.setPositionTarget(target_pose1.position.x,  target_pose1.position.y, target_pose1.position.z, "link5");
-    group.setApproximateJointValueTarget(target_pose1,"link5");// only works with this function
-    // group.setGoalTolerance(0.05);
+    // group.setPositionTarget(target_pose1.position.x,  target_pose1.position.y, target_pose1.position.z, "link5");
+    // group.setApproximateJointValueTarget(target_pose1,"link5");// only works with this function
+    // // group.setGoalTolerance(0.05);
     
-    success = group.plan(my_plan);
-    // ROS_INFO("Visualizing plan 1 (pose goal) %s", success.val ? "" : "FAILED");
-          //If cannot find plan
-    if (!success)
-      ROS_INFO("fail to return above");
-          //If find plan      
-    else
-      group.move(); //blocking
-      sleep(5.0);
+    // success = group.plan(my_plan);
+    // // ROS_INFO("Visualizing plan 1 (pose goal) %s", success.val ? "" : "FAILED");
+    //       //If cannot find plan
+    // if (!success)
+    //   ROS_INFO("fail to return above");
+    //       //If find plan      
+    // else
+    //   group.move(); //blocking
+    //   sleep(5.0);
 
 // ///////////////////////////////////////////////////////////////////////////////////move to home      
   joint_model_group = group.getCurrentState()->getJointModelGroup("arm");
@@ -151,9 +171,19 @@ void server::goalCB(){
   group.setJointValueTarget(joint_group_positions);
   group.setGoalTolerance(0.05);
 
-  group.move(); //blocking
-  // sleep(5.0);
-  ROS_INFO("home");
+    if (!success){
+      feedback.feedback_code.feedback_code = "fail to move home";
+      result.result.result_code = "plan fail";
+      as->publishFeedback(feedback);
+      as->setAborted(result);
+ 
+    }
+    else{//If find plan
+      feedback.feedback_code.feedback_code = "move to move home";
+      as->publishFeedback(feedback);  
+      group.move(); //blocking
+      sleep(5.0);
+    }
 
 // ///////////////////////////////////////////////////////////////////////////////////move to loco   
   joint_model_group = group.getCurrentState()->getJointModelGroup("arm");
@@ -170,9 +200,19 @@ void server::goalCB(){
   group.setJointValueTarget(joint_group_positions1);
   group.setGoalTolerance(0.05);
 
-  group.move(); //blocking
-  sleep(5.0);
-  ROS_INFO("home");  
+    if (!success){
+      feedback.feedback_code.feedback_code = "fail to move loco position";
+      result.result.result_code = "plan fail";
+      as->publishFeedback(feedback);
+      as->setAborted(result);
+ 
+    }
+    else{//If find plan
+      feedback.feedback_code.feedback_code = "move to move loco position";
+      as->publishFeedback(feedback);  
+      group.move(); //blocking
+      sleep(5.0);
+    }
 
 }
 
